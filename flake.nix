b744443a1e92116e5213e9828cb7d4e38d3ab52d
Lucas Ribeiro <patruop@gmail.com>
2026-01-14 20:06:34 -0300

    use lld and clang

--- flake.nix
+++ flake.nix
@@ -42,22 +42,15 @@
                 vulkan-tools
                 # Other dependencies
                 libudev-zero
-                xorg.libX11
-                xorg.libXcursor
-                xorg.libXi
-                xorg.libXrandr
-                libxkbcommon
                 bacon
                 wayland
                 cargo-flamegraph
+                clang
+                lld
               ];
             RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
             LD_LIBRARY_PATH = lib.makeLibraryPath [
               vulkan-loader
-              xorg.libX11
-              xorg.libXi
-              xorg.libXcursor
-              libxkbcommon
             ];
           };
       }
